f5aa55893976b1ff6ad4d6c8ed81e90ae092680e
Sebastian Mendoza <sebastianmendoza482@gmail.com>
2020-08-12 14:30:12 -0700

    Function corrected

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
-votingAge = 20
-if (votingAge > 18) {
+votingAge = 18
+if (votingAge >= 18) {
     console.log("true")
 } else {
     console.log("false");
@@ -14,45 +14,53 @@ let second = 2;
 if (second > first){ 
     first = (first + 1)
 } 
+console.log(first);
+console.log(second);
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 let string = "1999";
-Number(string);
-
+console.log(string);
+string = Number(string);
+console.log(string);
 /* Task D */
 //Task d: Write a function to multiply a*b 
 function multiply (a,b){
   return (a*b);
 }
+console.log(multiply(3,7))
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
 function dogAge(myAge){
-  return(myAge)*7;
-}
+  return myAge*7;
+} 
+console.log(dogAge(10));
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
 //takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
 function dogFeeder(weight,age){
-  /*WEIGHT*/
-  if (weight<=5){
-    console.log("5% of their body weight");
-  } else if (weight <=10){
-    console.log("4% of their body weight");
+  if (age>=2 || age<=4){
+    return weight*0.1;
+  } else if (age>4 || age<=7){
+    return weight*0.05
+  } else if (age>7 || age<=12){
+    return weight*0.04
+  } else if (weight<=5){
+    return weight*0.05;
+  } else if (weight<=10){
+    return weight*0.04
   } else if (weight<=15){
-    console.log("3% of their body weight");
+    return weight*0.03
   } else if (weight>15){
-    console.log("2% of their body weight");
-  /*AGE*/
-  } else if (age<4){
-    console.log("10% of their body weight")
-  } else if (age<7){
-    console.log("5% of their body weight")
-  } else if (age<12){
-    console.log("4% of their body weight")
-  } 
+    return weight*0.02
+  }
 }
+const dogMessage = dogFeeder(15);
+console.log(dogMessage);
+
+
+
 //feeding requirements
 // adult dogs at least 5 pounds
 // up to 5 lbs - 5% of their body weight
@@ -72,11 +80,12 @@ function dogFeeder(weight,age){
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
-var computerChoice = Math.random();
+
 //0.34<rock
 //0.67<paper
 //1<scissors
 function rockPaperScissors (myChoice,computerChoice){
+  computerChoice = Math.random();
   if (myChoice === "rock" && computerChoice <= 0.33){
     console.log("Computer chooses rock and it is a tie")
   } else if (myChoice === "rock" && computerChoice <= 0.67){
@@ -95,6 +104,14 @@ function rockPaperScissors (myChoice,computerChoice){
     console.log("Computer chooses rock and wins");
   } else if (myChoice === "scissors" && computerChoice<=0.67){
     console.log("Computer chooses paper and loses");
+  }
+}
+console.log(rockPaperScissors("rock"))
+console.log(rockPaperScissors("scissors"))
+console.log(rockPaperScissors("paper"))
+console.log(rockPaperScissors("paper"))
+
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
